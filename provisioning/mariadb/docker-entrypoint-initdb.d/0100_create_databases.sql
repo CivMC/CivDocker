07e815cf7a4a5e7fa67494990b524a253cfc8380
AngrySoundTech <angrysoundtech@github.com>
2022-05-25 12:43:08 -0400

    separate plugin databases

---
+++ provisioning/mariadb/docker-entrypoint-initdb.d/0100_create_databases.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE donum;
+CREATE DATABASE banstick;
+CREATE DATABASE bastion;
+CREATE DATABASE citadel;
+CREATE DATABASE civchat2;
+CREATE DATABASE civduties;
+CREATE DATABASE jukealert;
+CREATE DATABASE namelayer;
+CREATE DATABASE civmodcore;
+CREATE DATABASE castlegates;
+CREATE DATABASE essenceglue;
+CREATE DATABASE exilepearl;
+CREATE DATABASE realisticbiomes;
+
+GRANT ALL PRIVILEGES ON `donum`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `banstick`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `bastion`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `citadel`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `civchat2`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `civduties`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `jukealert`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `namelayer`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `civmodcore`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `castlegates`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `essenceglue`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `exilepearl`.* TO 'mariadb'@'%';
+GRANT ALL PRIVILEGES ON `realisticbiomes`.* TO 'mariadb'@'%';
